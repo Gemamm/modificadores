3b18f0f9e5da5af2420691dee4ff6bae0a1aab88
Gemamm <91058451+Gemamm@users.noreply.github.com>
2021-10-07 10:29:41 +0200

    Initial commit

---
+++ src/paquete3/Clase3.java
@@ -0,0 +1,9 @@
+
+package paquete3;
+
+import paquete1.Clase1;
+
+
+public class Clase3 extends Clase1 {
+    
+}
---
+++ src/test/Test.java
@@ -0,0 +1,12 @@
+
+package test;
+
+import paquete1.Clase1;
+
+
+public class Test {
+    public static void main(String[] args) {
+        Clase1 class1 = new Clase1();
+        System.out.println(Clase1.atPublic);
+    }
+}
