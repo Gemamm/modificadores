3d91527712378d44079e42494e4fc7a368dfd429
gemamm <gemamm1988@gmail.com>
2021-10-13 09:51:18 +0200

    Cambios

--- src/paquete1/Clase1.java
+++ src/paquete1/Clase1.java
@@ -3,5 +3,17 @@ package paquete1;
 
 
 public class Clase1 {
-    public static int atPublic;
+    protected String atProtegido;
+    
+    protected Clase1(){
+        System.out.println("Imprimiendo desde el constructor vacio protegido clase 1");
+    }
+    
+    public Clase1(String arg){
+        System.out.println("Desde el constructor publico con  1 argumento"+ arg);
+    }
+    
+    protected void metodoProtegido(){
+        System.out.println("Imprimiendo desde metodo protegido.");
+    }
 }
--- src/paquete3/Clase3.java
+++ src/paquete3/Clase3.java
@@ -6,4 +6,13 @@ import paquete1.Clase1;
 
 public class Clase3 extends Clase1 {
     
+    public Clase3(){
+        super();
+        this.atProtegido = "Modificamos el atributo protegido";
+        System.out.println("Atributo publico hija" +
+        this.atProtegido);
+        this.metodoProtegido();
+        
+    }
+    
 }
